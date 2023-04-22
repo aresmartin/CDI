142b317d761fadc1d96324c40e4b657806014a11
aresmartin <127427815+aresmartin@users.noreply.github.com>
2023-04-22 16:01:35 +0200

    Finalizada Practica 1

---
+++ Practica1/Pruebas.java
@@ -0,0 +1,71 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Pruebas {
+    
+    public static void main(String args[]){
+
+        int primerArgumento = Integer.valueOf(args[0]);
+        System.out.println("Hello i am number: " + Thread.currentThread().getName());
+        /*for(int i = 0; i < primerArgumento ; i++){
+            
+            MultithreadThing myThings =  new MultithreadThing();
+            myThings.start();
+        } */
+        List<Thread> myList = new ArrayList<>(primerArgumento);
+        for(int i = 0; i < primerArgumento ; i++){
+           Thread myThread = new Thread(new PruebaRunnable());
+           myList.add(i, myThread);          
+        }
+
+        for(int i= 0; i < primerArgumento ; i++){
+              myList.get(i).start();        
+         }
+
+        System.out.println("Bye i am number: " + Thread.currentThread().getName());
+    }
+
+}
+
+ class PruebaRunnable implements Runnable{
+
+    public void run(){
+        System.out.println("Hello i am number: " + Thread.currentThread().getName());
+        for(int i = 0; i <= 5; i++){
+            System.out.println(i);
+
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+
+            }
+
+        }
+        System.out.println("Bye i am number: " + Thread.currentThread().getName());
+    }
+}
+
+     class MultithreadThing extends Thread{
+        
+        //@Override
+        public void run(){
+            System.out.println("Hello i am number: " + getName());
+
+            for(int i = 0; i <= 5; i++){
+                System.out.println(i);
+    
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+    
+                }
+    
+            }
+            System.out.println("Hello i am number: " + getName());
+        }
+    }
+
+
+
+
+
